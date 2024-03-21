42eb8c950d0d013318c28417e85b0783f486427a
Jeffrey <jeff.boesenkool@gmail.com>
2024-03-21 23:33:44 +0100

    fmt

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,7 @@
 /** @type { import("eslint").Linter.Config } */
 module.exports = {
     root: true,
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:svelte/recommended',
-        'prettier'
-    ],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:svelte/recommended', 'prettier'],
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
     parserOptions: {
@@ -28,14 +23,5 @@ module.exports = {
             }
         }
     ],
-    ignorePatterns: [
-        '.DS_Store',
-        'Thumbs.db',
-        '.idea/',
-        '.vscode/',
-        'build/',
-        'node_modules/',
-        '.svelte-kit/',
-        'pnpm-lock.yaml'
-    ]
+    ignorePatterns: ['.DS_Store', 'Thumbs.db', '.idea/', '.vscode/', 'build/', 'node_modules/', '.svelte-kit/', 'pnpm-lock.yaml']
 };
