d65141d9b94e2157e97e3aba7da4606e4021f692
Jeffrey <jeff.boesenkool@gmail.com>
2024-03-27 16:00:15 +0100

    init simple footer

---
+++ src/lib/layout/footer.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+
+</script>
+
+<footer>
+    <div class="container">
+        <div class="info">
+            <span><b>tukkr</b></span>
+            <span><a href="mailto:info@tukkr.dev">info@tukkr.dev</a></span>
+            <span><a href="/contact">More information</a></span>
+        </div>
+        <nav>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/snippets">Snippets</a>
+            <a href="/contact">Contact</a>
+        </nav>
+        <div class="socials">
+            <a href="https://www.linkedin.com/in/jeffrey-boesenkool/" target="_blank">LinkedIn</a>
+            <a href="https://github.com/JeffreyTheTukkr" target="_blank">GitHub</a>
+        </div>
+    </div>
+</footer>
+
+<style lang="scss">
+    footer {
+        background-color: #000;
+        padding: 3rem 0;
+
+        // general layout
+        .container {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            gap: 1rem;
+
+            > div, > nav {
+                display: inherit;
+                flex-direction: column;
+                gap: 1rem;
+            }
+        }
+    }
+</style>
