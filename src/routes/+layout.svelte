956b802aa782d46f86ec05852de71544ef99c614
Jeffrey <jeff.boesenkool@gmail.com>
2024-03-27 16:00:24 +0100

    include footer in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
     import '@/scss/global.scss';
+    
+    import FooterLayout from '$lib/layout/footer.svelte';
 </script>
 
 <main>
     <slot />
 </main>
+<svelte:component this={FooterLayout} />
