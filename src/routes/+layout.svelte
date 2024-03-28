77cac84a20cd2722cdb3b5fafc03cd2ec73e92b5
Jeffrey <jeff.boesenkool@gmail.com>
2024-03-28 10:39:37 +0100

    add header to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
     import '@/scss/global.scss';
     
+    import HeaderLayout from '$lib/layout/header.svelte'
     import FooterLayout from '$lib/layout/footer.svelte';
 </script>
 
+<svelte:component this={HeaderLayout} />
 <main>
     <slot />
 </main>
