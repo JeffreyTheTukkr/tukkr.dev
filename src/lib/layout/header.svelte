a013db216f688fe6c8a8d1e7fc9c1fe390a10f43
Jeffrey <jeff.boesenkool@gmail.com>
2024-03-28 23:37:43 +0100

    remove redundent a11y role

--- src/lib/layout/header.svelte
+++ src/lib/layout/header.svelte
@@ -37,7 +37,7 @@
             <path d="m 30,67 h 40 c 0,0 8.5,0.149796 8.5,-8.5 0,-8.649796 -8.5,-8.5 -8.5,-8.5 h -20 v 20" />
         </svg>
     </button>
-    <nav aria-label="Main menu" class:active={mobileMenuActive} id="main-menu" role="navigation">
+    <nav aria-label="Main menu" class:active={mobileMenuActive} id="main-menu">
         <a class:active={isCurrentPage('/')} href="/">Home</a>
         <a class:active={isCurrentPage('/about')} href="/about">About</a>
         <a class:active={isCurrentPage('/snippets')} href="/snippets">Snippets</a>
