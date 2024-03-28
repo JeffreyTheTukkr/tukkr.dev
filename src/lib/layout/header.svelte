f1d0d1c621be8ead0395e7013df5d8786445a147
Jeffrey <jeff.boesenkool@gmail.com>
2024-03-28 10:39:31 +0100

    add basic header

---
+++ src/lib/layout/header.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    
+</script>
+
+<header>
+    <div class="logo">
+        
+    </div>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/snippets">Snippets</a>
+        <a href="/contact">Contact</a>
+    </nav>
+</header>
+
+<style lang="scss">
+    header {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        padding: 2rem 3rem;
+        background-color: rgba(0, 0, 0, 0.6);
+        
+        .logo {
+            
+        }
+        
+        nav {
+            display: flex;
+            flex-direction: row;
+            gap: 1rem;
+        }
+    }
+</style>
