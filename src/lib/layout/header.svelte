199e76f88241b35777a0e81c2f35d262b396e936
Jeffrey <jeff.boesenkool@gmail.com>
2024-03-28 14:06:51 +0100

    fix paddings

--- src/lib/layout/header.svelte
+++ src/lib/layout/header.svelte
@@ -4,7 +4,7 @@
 <header>
     <div class="logo">
         <a href="/">
-            <img src="/logo.svg" alt="tukkr logo" height="44" />
+            <img src="/logo.svg" alt="tukkr logo" height="40" />
         </a>
     </div>
     <nav>
@@ -21,7 +21,7 @@
         flex-direction: row;
         justify-content: space-between;
         align-items: center;
-        padding: 1.6rem 3rem;
+        padding: 2rem;
         background-color: rgba(0, 0, 0, 0.6);
 
         .logo {
@@ -31,6 +31,10 @@
             display: flex;
             flex-direction: row;
             gap: 1rem;
+
+            a {
+                padding: 0.4rem;
+            }
         }
     }
 </style>
