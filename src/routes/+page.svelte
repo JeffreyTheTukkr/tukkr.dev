a948aa69fd222cf2e785861c5ce04320d8dd21d0
Jeffrey <jeff.boesenkool@gmail.com>
2024-05-16 17:34:40 +0200

    add page content and styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,59 @@
     import SEO from '$lib/components/seo.svelte';
 </script>
 
-<SEO title="Fullstack Web Developer" description="Hey, I am Jeffrey, a Fullstack Web Developer from The Netherlands." />
+<SEO title="Fullstack Web Developer" description="Hey! I am Jeffrey, a Fullstack Web Developer from The Netherlands." />
 
 <div class="container">
     <h1>tukkr</h1>
-    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    <p>Hey! I am Jeffrey, a Fullstack Web Developer from The Netherlands.</p>
+    <div class="page-actions">
+        <a href="/about">
+            <div class="about">
+                <h2>About me</h2>
+                <p>Read more about me and my past experiences.</p>
+            </div>
+        </a>
+        <a href="/snippets">
+            <div class="snippets">
+                <h2>Snippets</h2>
+                <p>The place where I store some cool snippets / knowledge.</p>
+            </div>
+        </a>
+    </div>
 </div>
+
+<style lang="scss">
+    .page-actions {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        gap: 2rem;
+        margin-top: 2rem;
+
+        @include mediaQuery(s) {
+            flex-direction: row;
+        }
+
+        > a {
+            padding: 1rem;
+            border: 1px solid $color-border-primary;
+            border-radius: 16px;
+            transition: border-color 0.12s ease-in-out;
+
+            @include mediaQuery(s) {
+                width: calc((100% - 2rem) / 2);
+            }
+
+            &:hover {
+                border-color: $color-border-dark;
+            }
+
+            // restore heading styling to normal text
+            h2 {
+                margin: 0;
+                color: inherit;
+                font-size: inherit;
+            }
+        }
+    }
+</style>
