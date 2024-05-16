017a067fa1e09dc194ff38660937b953fb1c78e0
Jeffrey <jeff.boesenkool@gmail.com>
2024-05-16 22:12:51 +0200

    add basic snippets page

---
+++ src/routes/snippets/+page.svelte
@@ -0,0 +1,4 @@
+<div class="container">
+    <h1>Snippets</h1>
+    <p>While I'm not big on blog writing, I do occasionally come across some neat pieces of code or specific knowledge which I find worth sharing and storing.</p>
+</div>
