401ae3c3f56ce13728e22c1733f8d5e8f66db326
Jeffrey <jeff.boesenkool@gmail.com>
2024-05-17 17:52:54 +0200

    change code highligher

--- src/routes/snippets/[...path]/+page.server.ts
+++ src/routes/snippets/[...path]/+page.server.ts
@@ -6,9 +6,17 @@ export const load: ServerLoad = async ({ url }): Promise<{ post: PostI }> => {
     // retrieves the second string path part after `/` to match the mdx filename
     const lastPath: string = url.pathname.split('/')[2];
     const post: PostI = getPostBySlug(lastPath);
-    const content = await compile(post.content);
+    const content = await compile(post.content, {
+        highlight: { highlighter }
+    });
 
     return {
         post: { ...post, content: content?.code ?? '' }
     };
+};
+
+const highlighter = (code: string, lang?: string): string => {
+    const className: string = 'language-' + lang;
+
+    return `<pre><code class=${className}>${code}</code></pre>`;
 };
