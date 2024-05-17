f5f4a315f1b9b1cf22ad3a183bb7b153cbebb16f
Jeffrey <jeff.boesenkool@gmail.com>
2024-05-17 15:34:57 +0200

    add page content

---
+++ src/routes/snippets/[...path]/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import SEO from '$lib/components/seo.svelte';
+    import type { PageData } from './$types';
+
+    export let data: PageData;
+</script>
+
+<SEO title={data.post.title} description={data.post.description} />
+
+<div class="container">
+    <h1>{data.post.title}</h1>
+    {@html data.post.content}
+</div>
