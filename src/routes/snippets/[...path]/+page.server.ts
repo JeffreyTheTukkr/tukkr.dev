0dd79c24eb2163fcea7a7c1a465be47ebebcd991
Jeffrey <jeff.boesenkool@gmail.com>
2024-05-17 16:36:55 +0200

    compile mdx content

--- src/routes/snippets/[...path]/+page.server.ts
+++ src/routes/snippets/[...path]/+page.server.ts
@@ -1,12 +1,14 @@
 import type { ServerLoad } from '@sveltejs/kit';
 import { getPostBySlug, type PostI } from '$lib/server/mdx';
+import { compile } from 'mdsvex';
 
-export const load: ServerLoad = ({ url }): { post: PostI } => {
+export const load: ServerLoad = async ({ url }): Promise<{ post: PostI }> => {
     // retrieves the second string path part after `/` to match the mdx filename
     const lastPath: string = url.pathname.split('/')[2];
     const post: PostI = getPostBySlug(lastPath);
+    const content = await compile(post.content);
 
     return {
-        post: post
+        post: { ...post, content: content?.code ?? '' }
     };
 };
