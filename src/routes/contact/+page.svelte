1e1ad36202a939ca4f290a531d9ba66b9b3f6713
Jeffrey <jeff.boesenkool@gmail.com>
2024-05-18 00:13:02 +0200

    remove redundant bold tags

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -9,14 +9,14 @@
     <p>Do you have questions or are you interested in working together? Feel free to send me a message!</p>
     <div class="content">
         <div class="company">
-            <h2><b>Company details</b></h2>
+            <h2>Company details</h2>
             <p><b>Name:</b> tukkr</p>
             <p><b>Coc:</b> 92668828</p>
             <p><b>Vat:</b> NL004968891B61</p>
             <p><b>Address:</b><br />De Dorsmolen 47<br />7623HT Borne<br />The Netherlands</p>
         </div>
         <div class="contact">
-            <h2><b>Contact details</b></h2>
+            <h2>Contact details</h2>
             <p><b>Email:</b> <a href="mailto:info@tukkr.dev">info@tukkr.dev</a></p>
             <p><b>Phone:</b> <a href="tel:+31642168407">+31 (0)6 421 684 07</a></p>
             <p><b>LinkedIn:</b> <a href="https://www.linkedin.com/in/jeffrey-boesenkool/" target="_blank">Jeffrey Boesenkool</a></p>
