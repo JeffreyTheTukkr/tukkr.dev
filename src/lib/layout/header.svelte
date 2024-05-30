6d32e3900c1fa93c7d906658f463aa36f44a7930
Jeffrey <jeff.boesenkool@gmail.com>
2024-05-30 11:19:42 +0200

    increase opacity

--- src/lib/layout/header.svelte
+++ src/lib/layout/header.svelte
@@ -60,7 +60,7 @@
         align-items: center;
         will-change: padding;
         padding: 1.8rem 2rem;
-        background-color: rgba(255, 255, 255, 0.9);
+        background-color: rgba(255, 255, 255, 0.94);
         transition: padding 0.2s ease-in-out;
         z-index: 999;
 
