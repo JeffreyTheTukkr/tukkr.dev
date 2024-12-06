b5baa757cafe24e7ae1fbd24df4e1c3e9f8a02e8
Jeffrey <39796883+JeffreyTheTukkr@users.noreply.github.com>
2024-12-06 13:03:02 +0100

    add todo item

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -13,6 +13,8 @@ Sentry.init({
 });
 
 // If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
+// todo: allow `injectFetchProxyScript` when issue in ticket below is resolved
+// https://github.com/getsentry/sentry-javascript/pull/9969
 export const handle = sequence(sentryHandle({ injectFetchProxyScript: false }));
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
