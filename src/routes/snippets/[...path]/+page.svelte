111e8363cf0f0a7f3a496e36a67e023a8350b08f
Jeffrey <39796883+JeffreyTheTukkr@users.noreply.github.com>
2024-12-06 11:45:17 +0100

    move eslint rule exclusion to more logical place

--- src/routes/snippets/[...path]/+page.svelte
+++ src/routes/snippets/[...path]/+page.svelte
@@ -3,8 +3,6 @@
     import type { PageData } from './$types';
 
     export let data: PageData;
-
-    /* eslint svelte/no-at-html-tags: "off" */
 </script>
 
 <SEO title={data.post.title} description={data.post.description} />
@@ -12,6 +10,7 @@
 <div class="container">
     <h1>{data.post.title}</h1>
     <div class="content">
+        <!-- eslint-disable-next-line svelte/no-at-html-tags -->
         {@html data.post.content}
     </div>
 </div>
